818998c8e4d035d3c4b280172c625951d5ec40f1
Fauzan Arif Sani <arifsanimail@gmail.com>
2023-08-25 03:16:31 +0700

    command experiment

--- discord_bot.py
+++ discord_bot.py
@@ -1,57 +1,58 @@
 import datetime
 import discord
-import requests
-import re
+from discord.ext import commands
 import math
 import os
-from discord.ext import commands
 from bs4 import BeautifulSoup
+import re
+import requests
 
 discord_token = os.environ['DISCORD_TOKEN']
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)  # Set the bot's command prefix
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+class CalculateCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-@bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
-    
-@bot.slash_command(name="calculate", description="Calculate TP-related information")
-async def calculate(ctx, username: str):
-    await ctx.send("Please wait...")
-    response = requests.get(f'https://account.aq.com/CharPage?id={username}')
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # Use regular expression to extract the ccid value from JavaScript
-        ccid_script = soup.find('script', string=re.compile(r'var ccid = \d+;'))
-        if ccid_script:
-            ccid_content = ccid_script.string
-            ccid_match = re.search(r'var ccid = (\d+);', ccid_content)
-            if ccid_match:
-                ccid = int(ccid_match.group(1))
-                # Now make a request to the API endpoint with the obtained ccid
-                inventory_url = f"https://account.aq.com/CharPage/Inventory?ccid={ccid}"
-                inventory_response = requests.get(inventory_url)
-                if inventory_response.status_code == 200:
-                    inventory_data = inventory_response.json()
-                    target_item_name = "Treasure Potion"
-                    target_item_entry = next((item for item in inventory_data if item.get("strName") == target_item_name), None)
-                    if target_item_entry:
-                        int_count = target_item_entry.get("intCount")
-                        await ctx.send(f"`{username.title()} current TP = {int_count} TP`")
-                        if int_count >= 1000:
-                            await ctx.send("```Gausah Sok Merendah Puh, Dah Dapet Wioda Itu```")
-                        else:
-                            if int_count < 10:
-                                await ctx.send("```Buset Abis Redeem Wioda Nih```")
-                                if int_count > 900:
+    @commands.command(name="calculate", description="Calculate TP-related information")
+    async def calculate(self, ctx, username: str):
+        await ctx.send("Please wait...")
+
+        response = requests.get(f'https://account.aq.com/CharPage?id={username}')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            
+            # Use regular expression to extract the ccid value from JavaScript
+            ccid_script = soup.find('script', string=re.compile(r'var ccid = \d+;'))
+            if ccid_script:
+                ccid_content = ccid_script.string
+                ccid_match = re.search(r'var ccid = (\d+);', ccid_content)
+                if ccid_match:
+                    ccid = int(ccid_match.group(1))
+
+                    # Now make a request to the API endpoint with the obtained ccid
+                    inventory_url = f"https://account.aq.com/CharPage/Inventory?ccid={ccid}"
+                    inventory_response = requests.get(inventory_url)
+
+                    if inventory_response.status_code == 200:
+                        inventory_data = inventory_response.json()
+                        target_item_name = "Treasure Potion"
+                        target_item_entry = next((item for item in inventory_data if item.get("strName") == target_item_name), None)
+                        if target_item_entry:
+                            int_count = target_item_entry.get("intCount")
+                            await ctx.send(f"`{username.title()} current TP = {int_count} TP`")
+                            if int_count >= 1000:
+                                await ctx.send("```Gausah Sok Merendah Puh, Dah Dapet Wioda Itu```")
+                            else:
+                                if int_count < 10:
+                                    await ctx.send("```Buset Abis Redeem Wioda Nih```")
+                                elif int_count > 900:
                                     await ctx.send("```Bentar Lagi Dapet Wioda Nih```")
+                                else:
                                     target_count = 1000
                                     # Calculate days and ACS for 2X TP/Spin
                                     await calculate_and_send_results(ctx, target_count, int_count, daily_gain=2, weekly_bonus=2, cost_per_potion=200)
@@ -60,18 +61,18 @@ async def calculate(ctx, username: str):
                                     await calculate_and_send_results(ctx, target_count, int_count, daily_gain=6, weekly_bonus=6, cost_per_potion=200)
                                     
                                     await ctx.send("```From Captive with ❤️\nCredit by Zou```")
+                        else:
+                            await ctx.send(f"{target_item_name} not found in inventory.")
                     else:
-                        await ctx.send(f"{target_item_name} not found in inventory.")
+                        await ctx.send("Failed to fetch inventory data.")
                 else:
-                    await ctx.send("Failed to fetch inventory data.")
+                    await ctx.send("ccid value not found in the script.")
             else:
-                await ctx.send("ccid value not found in the script.")
                 await ctx.send("Character Not Found.")
         else:
             await ctx.send("Failed to fetch data from the website.")
-                                    
 
-async def calculate_and_send_results(channel, target_count, int_count, daily_gain, weekly_bonus, cost_per_potion):
+async def calculate_and_send_results(ctx, target_count, int_count, daily_gain, weekly_bonus, cost_per_potion):
     current_date = datetime.datetime.now()
     
     days_per_week = 7
@@ -95,6 +96,7 @@ async def calculate_and_send_results(channel, target_count, int_count, daily_gai
     spin_needed = (target_count - int_count) / daily_gain
     rounded_spin = math.ceil(spin_needed)
     
-    await channel.send(f"```{daily_gain}X/Spin\nLegend = {rounded_days_legend} days ({future_date_legend_formatted})\nNon-Legend = {rounded_days_non_legend} days ({future_date_non_legend_formatted})\nWith ACS = {rounded_spin} Spin / {rounded_acs} ACS```")
+    await ctx.send(f"```{daily_gain}X/Spin\nLegend = {rounded_days_legend} days ({future_date_legend_formatted})\nNon-Legend = {rounded_days_non_legend} days ({future_date_non_legend_formatted})\nWith ACS = {rounded_spin} Spin / {rounded_acs} ACS```")
 
+bot.add_cog(CalculateCog(bot))
 bot.run(discord_token)
